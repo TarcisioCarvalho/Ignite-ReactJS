7d314a0bf83c3819db117a49df2d977caaf83e43
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2023-11-04 16:52:45 -0300

    module.css

---
+++ aulas/fudamentos-reactjs/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './styles.module.css';
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+        Feed
+    </header>
+  )
+}
