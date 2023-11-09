800dccfd969afae499242ae87c324c9ee76cad93
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2023-11-08 21:09:03 -0300

    finish sidebar

--- aulas/fudamentos-reactjs/src/components/SideBar/index.tsx
+++ aulas/fudamentos-reactjs/src/components/SideBar/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from "./styles.module.css";
+import { PencilLine } from 'phosphor-react';
 
 export const SideBar= () => {
   return (
@@ -7,11 +8,16 @@ export const SideBar= () => {
         <header></header>
         <img className={styles.cover} src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         <div className={styles.profile}>
+          <img src="https://github.com/tarcisioCarvalho.png" />
           <strong>Tarcísio Carvalho</strong>
           <span>Developer</span>
         </div>
         <footer>
-          <a href="">Editar seu perfil</a>
+         
+          <a href="">
+            <PencilLine size={20}/>
+            Editar seu perfil
+          </a>
         </footer>
         
     </aside>
