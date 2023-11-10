d055f4adc134704b85ba0e5e248ee47d137f4815
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2023-11-09 21:25:05 -0300

    begin post component

--- aulas/fudamentos-reactjs/src/components/Post/index.jsx
+++ aulas/fudamentos-reactjs/src/components/Post/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import styles from "./styles.module.css"
 
 export const Post = () => {
-  return (
-    <div>
-        Post
+  return <article className={styles.post}>
+    <header>
+      <div className={styles.author}>
+        <img className={styles.avatar} src="https://github.com/tarcisioCarvalho.png"/>
+        <strong>Tarcísio Carvalho</strong>
+        <span>Developer</span>
+      </div>
+     <time title="11 de maio de" dateTime="2022-05-11 08:13:30">
+      publica há 1 hora
+     </time>
+    </header>
+    <div className={styles.content}>
+      <p>Fala galeraa 👋</p>
+      <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+      <p><a href="">👉 jane.design/doctorcare</a></p>
+      <p><a href="">#novoprojeto #nlw #rocketseat </a></p>
     </div>
-  )
+  </article>
 }
