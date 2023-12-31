c2e94a96ca76ffecc4fe45b5c8aca76e098dc79f
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2023-12-31 17:14:10 -0300

    post

--- aulas/fudamentos-reactjs/src/App.jsx
+++ aulas/fudamentos-reactjs/src/App.jsx
@@ -89,6 +89,9 @@ function App() {
             <Post/>
             <Post/>
             <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
           </main>
           
       </div>
